cb26e212f6fcb9c8ffb1998a0bf34a172fbd683a
thechipper <jonathanthehigley@gmail.com>
2023-10-30 19:35:44 -0400

    hello

--- Lab6/main.py
+++ Lab6/main.py
@@ -1,12 +1,4 @@
 #encoder
-def decode(x):
-    num_string = x
-    num_list = []
-    for i in num_string:
-        num_list.append(str(int(i) + 3))
-    num_string = ''.join(num_list)
-    return num_string
-
 while True:
     menu_option = int(input("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\n\nPlease enter an option: "))
 
@@ -14,7 +6,8 @@ while True:
         stored_encode = input("Please enter your password to encode: ")
         print("Your password has been encoded and stored!\n\n")
     elif menu_option == 2:
-        decode_value = decode(stored_encode)
-        print(f"The encoded password is {decode_value} and the original password is {stored_encode}\n\n")
+        pass
+
     elif menu_option == 3:
         quit()
+
