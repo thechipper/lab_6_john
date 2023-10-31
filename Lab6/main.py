ba5c4b7ed62590a792a6362dc70a739b9702dc75
Kyle Stamm <stamm.kyle@ufl.edu>
2023-10-30 20:26:12 -0400

    kyle update

--- Lab6/main.py
+++ Lab6/main.py
@@ -8,14 +8,30 @@ def encode():
     return stored_decode
 
 
+def decode(encoded):
+
+    return_str = ''
+
+    for digit in range(0, len(encoded)):
+
+        if encoded[digit] - 3 >= 0:
+            return_str += str(encoded[digit] - 3)
+        else:
+            return_str += str(encoded[digit] - 3 + 9)
+
+    return return_str
+
+
+
 while True:
     menu_option = int(input("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n\n\nPlease enter an option: "))
 
     if menu_option == 1:
         stored_encode = encode()
+        print(stored_encode)
     elif menu_option == 2:
-        pass
-
+        decoded = decode(stored_encode)
+        print(decoded)
     elif menu_option == 3:
         quit()
 
