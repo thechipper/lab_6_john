1f9d3c57f34c75fab1de0e93b72c82170d271285
thechipper <jonathanthehigley@gmail.com>
2023-10-30 20:41:31 -0400

    'final'

--- Lab6/main.py
+++ Lab6/main.py
@@ -3,7 +3,10 @@ def encode():
     stored_encode = input("Please enter your password to encode: ")
     stored_decode = []
     for i in stored_encode:
-        stored_decode.append(int(i) + 3)
+        if (int(i) + 3) > 9:
+            stored_decode.append(int(i) + 3 - 10)
+        else:
+            stored_decode.append(int(i) + 3)
     print("Your password has been encoded and stored!\n\n")
     return stored_decode
 
@@ -17,7 +20,7 @@ def decode(encoded):
         if encoded[digit] - 3 >= 0:
             return_str += str(encoded[digit] - 3)
         else:
-            return_str += str(encoded[digit] - 3 + 9)
+            return_str += str(encoded[digit] - 3 + 10)
 
     return return_str
 
@@ -28,10 +31,12 @@ while True:
 
     if menu_option == 1:
         stored_encode = encode()
-        print(stored_encode)
     elif menu_option == 2:
         decoded = decode(stored_encode)
-        print(decoded)
+        temp_encode = ''
+        for i in stored_encode:
+            temp_encode += str(i)
+        print(f'The encoded password is {temp_encode}, and the original password is {decoded}')
     elif menu_option == 3:
         quit()
 
